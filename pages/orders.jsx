ca47f5807c2324a4acc7a637d5bca2f28e192029
SALVADORPOETA <dsml@live.com.mx>
2023-08-20 18:57:20 -0600

    spaces

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -7,8 +7,8 @@ const orders = () => {
   return (
       <div className='bg-gray-100 min-h-screen'>
         <div className='flex justify-between px-4 pt-4'>
-          <h2>Orders </h2>
-          <h2>Welcome back, Salvador</h2>
+          <h2>Orders</h2>
+          <h2> Welcome back, Salvador</h2>
         </div>
         <div className='p-4'>
           <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
