cf5e01f99e11e373732d668d92da0e795125e3d0
SALVADORPOETA <dsml@live.com.mx>
2023-08-20 19:00:29 -0600

    test1

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-4'>
-        <h2>Dashboard</h2>
-        <h2> Welcome back, Salvador1</h2>
+        <h2 className='mr-1'>Dashboard</h2>
+        <h2>Welcome back, Salvador</h2>
     </div>
   )
 }
--- pages/customers.jsx
+++ pages/customers.jsx
@@ -6,7 +6,7 @@ const customers = () => {
   return (
     <div className='bg-gray-100 min-h-screen'>
       <div className='flex justify-between p-4'>
-        <h2>Customers</h2>
+        <h2 className='mr-1'>Customers</h2>
         <h2> Welcome back, Salvador</h2>
       </div>
       <div className='p-4'>
